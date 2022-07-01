a6abda2901b3857e968e42d9069522ab2c58a5f6
Te1amon <72522683+Te1amon@users.noreply.github.com>
2022-07-01 15:47:44 -0400

    fixed stupid loader bug, has to manually be updated now :(

--- loader.lua
+++ loader.lua
@@ -1,7 +1,17 @@
-a = game:HttpGet("https://raw.githubusercontent.com/Te1amon/Reflex/main/"..game.PlaceId..".lua")
-print("a")
-wait()
-if a then
+supported = {"155615604", "286090429", "47324"}
+
+-- / / / / / / / / / / / / / / / / / / / /
+
+isgamesupported = false
+
+for i, v in pairs(supported) do
+    lol = tonumber(v)
+    if game.PlaceId == lol then
+        isgamesupported = true
+        end
+    end
+
+if isgamesupported then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Te1amon/Reflex/main/"..game.PlaceId..".lua"))() -- load reflex for the supported game
 else
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Te1amon/Reflex/main/universal.lua"))() -- load the universal script
