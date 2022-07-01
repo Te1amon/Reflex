1d75a90c04eae98d4278c4c8366975b3022bd288
Te1amon <72522683+Te1amon@users.noreply.github.com>
2022-07-01 17:24:12 -0400

    reverted back temporally

--- loader.lua
+++ loader.lua
@@ -1,4 +1,8 @@
 -- made by dex
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Te1amon/Reflex/main/"..game.PlaceId..".lua"))()
+
+--[[
 local success, res = pcall(function()
     return loadstring(game:HttpGet("https://raw.githubusercontent.com/Te1amon/Reflex/main/"..game.PlaceId..".lua"))() -- check for existing game
 end)
@@ -7,3 +11,4 @@ if not success or not res then
     return loadstring(game:HttpGet("https://raw.githubusercontent.com/Te1amon/Reflex/main/Universal.lua"))() -- load uni
 end
 res()
+]]
