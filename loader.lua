ee3ae555f7a7d9dc74e739141658f134ee35449f
BinaryDex <82985536+BinaryDex@users.noreply.github.com>
2022-07-01 16:05:00 -0400

    fixed by me (so pro)

--- loader.lua
+++ loader.lua
@@ -1,6 +1,6 @@
 -- made by dex
 local success, res = pcall(function()
-    return loadstring(game:HttpGet("https://raw.githubusercontent.com/Te1amon/Reflex/ain/"..game.PlaceId..".lua"))() -- check for existing game
+    return loadstring(game:HttpGet("https://raw.githubusercontent.com/Te1amon/Reflex/main/"..game.PlaceId..".lua"))() -- check for existing game
 end)
 
 if not success or not res then
