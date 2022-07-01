95cb3cfa6ec071c5035fef2b8d3e7d64363809b1
BinaryDex <82985536+BinaryDex@users.noreply.github.com>
2022-07-01 16:02:41 -0400

    fixed me me (so pro)

--- loader.lua
+++ loader.lua
@@ -1,18 +1,9 @@
-supported = {"155615604", "286090429", "47324"}
+-- made by dex
+local success, res = pcall(function()
+    return loadstring(game:HttpGet("https://raw.githubusercontent.com/Te1amon/Reflex/ain/"..game.PlaceId..".lua"))() -- check for existing game
+end)
 
--- / / / / / / / / / / / / / / / / / / / /
-
-isgamesupported = false
-
-for i, v in pairs(supported) do
-    lol = tonumber(v)
-    if game.PlaceId == lol then
-        isgamesupported = true
-        end
-    end
-
-if isgamesupported then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Te1amon/Reflex/main/"..game.PlaceId..".lua"))() -- load reflex for the supported game
-else
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Te1amon/Reflex/main/universal.lua"))() -- load the universal script
-    end
+if not success or not res then
+    return loadstring(game:HttpGet("https://raw.githubusercontent.com/Te1amon/Reflex/main/Universal.lua"))() -- load uni
+end
+res()
